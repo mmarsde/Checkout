ec224838cad0641793c24915b1be6606ee937672
mmarsde <57989672+mmarsde@users.noreply.github.com>
2024-04-17 22:58:52 +0100

    Add boiler plate. Add test to return zero, if no products scanned.

--- src/Checkout/Checkout.cs
+++ src/Checkout/Checkout.cs
@@ -1,6 +1,20 @@
 ﻿namespace Checkout
 {
-    internal class Checkout
+    public class Checkout : ICheckout
     {
+        private readonly List<string> _scannedItems = new List<string>();
+
+        public int GetTotalPrice()
+        {
+            if (_scannedItems.Count == 0)
+                return 0;
+
+            return 50;
+        }
+
+        public void Scan(string item)
+        {
+            _scannedItems.Add(item);
+        }
     }
 }
--- tests/Checkout.Tests/CheckoutTests.cs
+++ tests/Checkout.Tests/CheckoutTests.cs
@@ -1,11 +1,34 @@
+using FluentAssertions;
+
 namespace Checkout.Tests
 {
     public class CheckoutTests
     {
         [Fact]
-        public void Test1()
+        public void GetTotalPrice_NoItems_Returns_Zero()
         {
+            // Arrange
+            var sut = new Checkout();
 
+            // Act
+            var price = sut.GetTotalPrice();
+
+            // Assert
+            price.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetTotalPrice_ProductA_Returns_50()
+        {
+            // Arrange
+            var sut = new Checkout();
+            sut.Scan("A");
+
+            // Act
+            var price = sut.GetTotalPrice();
+
+            // Assert
+            price.Should().Be(50);
         }
     }
 }
